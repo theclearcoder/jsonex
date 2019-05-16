e1351245b587fd33d5edcfd0c79d13406a579b4a
Brian Gamble <brian@theclearcoder.com>
2019-05-15 21:35:57 -0500

    Implemented Jsonex.encode/1

--- lib/jsonex.ex
+++ lib/jsonex.ex
@@ -3,10 +3,29 @@ defmodule Jsonex do
   Documentation for Jsonex.
   """
 
-  def encode(str) do
-    # Your code goes here
-    :TODO_IMPLEMENT_ME
+  def encode(nil), do: "null"
+
+  def encode(str) when is_tuple(str) or is_pid(str) or is_float(str) do
+    raise Jsonex.BadInputError, str
   end
+
+  def encode(str) when is_boolean(str), do: to_string(str)
+
+  def encode(str) when is_binary(str) or is_atom(str), do: escaped(str)
+
+  def encode(str) when is_map(str) do
+    "{#{Enum.map_join(str, ",", fn {k, v} -> encode(k, v) end)}}"
+  end
+
+  def encode(str) when is_list(str) do
+    "[#{Enum.map_join(str, ",", &encode/1)}]"
+  end
+
+  def encode(str), do: to_string(str)
+
+  def encode(str1, str2), do: "#{encode(str1)}:#{encode(str2)}"
+
+  defp escaped(str), do: "\"#{to_string(str)}\""
 end
 
 defmodule Jsonex.BadInputError do
