a7ba238ee0e3332dd995ec4aa05c67245d9b89a9
Brian Gamble <brian@theclearcoder.com>
2019-05-15 23:43:33 -0500

    Rename str -> term

--- lib/jsonex.ex
+++ lib/jsonex.ex
@@ -5,27 +5,27 @@ defmodule Jsonex do
 
   def encode(nil), do: "null"
 
-  def encode(str) when is_tuple(str) or is_pid(str) or is_float(str) do
-    raise Jsonex.BadInputError, str
+  def encode(term) when is_tuple(term) or is_pid(term) or is_float(term) do
+    raise Jsonex.BadInputError, term
   end
 
-  def encode(str) when is_boolean(str), do: to_string(str)
+  def encode(term) when is_boolean(term), do: to_string(term)
 
-  def encode(str) when is_binary(str) or is_atom(str), do: escaped(str)
+  def encode(term) when is_binary(term) or is_atom(term), do: escaped(term)
 
-  def encode(str) when is_map(str) do
-    "{#{Enum.map_join(str, ",", fn {k, v} -> encode(k, v) end)}}"
+  def encode(term) when is_map(term) do
+    "{#{Enum.map_join(term, ",", fn {k, v} -> encode(k, v) end)}}"
   end
 
-  def encode(str) when is_list(str) do
-    "[#{Enum.map_join(str, ",", &encode/1)}]"
+  def encode(term) when is_list(term) do
+    "[#{Enum.map_join(term, ",", &encode/1)}]"
   end
 
-  def encode(str), do: to_string(str)
+  def encode(term), do: to_string(term)
 
-  def encode(str1, str2), do: "#{encode(str1)}:#{encode(str2)}"
+  def encode(term1, term2), do: "#{encode(term1)}:#{encode(term2)}"
 
-  defp escaped(str), do: "\"#{to_string(str)}\""
+  defp escaped(term), do: "\"#{to_string(term)}\""
 end
 
 defmodule Jsonex.BadInputError do
